92baff4696c30caa2eff85e103429e2a69d54fa8
bleistivt <bleistivt@users.noreply.github.com>
2015-06-27 15:10:50 +0200

    casening

--- views/delete.php
+++ views/delete.php
@@ -1,15 +1,15 @@
 <?php if (!defined('APPLICATION')) exit(); 
 
-echo Wrap($this->Data('Title'), 'h1');
-echo $this->Form->Open();
-echo $this->Form->Errors();
+echo wrap($this->data('Title'), 'h1');
+echo $this->Form->open();
+echo $this->Form->errors();
 echo '<div class="P">'.sprintf(
     T('Are you sure you want to take away <em style="font-style:italic;">%s</em> from %s? If the the conditions for receiving this badge are still met, it will be awarded again.'),
-    $this->Data('Badgename'),
-    $this->Data('Username')
+    $this->data('Badgename'),
+    $this->data('Username')
 ).'</div>';
 echo '<div class="Buttons Buttons-Confirm">';
-echo $this->Form->Button('OK', array('class' => 'Button Primary'));
-echo $this->Form->Button('Cancel', array('type' => 'button', 'class' => 'Button Close'));
+echo $this->Form->button('OK', array('class' => 'Button Primary'));
+echo $this->Form->button('Cancel', array('type' => 'button', 'class' => 'Button Close'));
 echo '</div>';
-echo $this->Form->Close();
+echo $this->Form->close();
