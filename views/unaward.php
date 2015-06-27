ad39527a9252ec0ddbadfabb78f91cf43f7cf2a6
bleistivt <bleistivt@users.noreply.github.com>
2015-06-27 15:13:06 +0200

    casening

--- views/unaward.php
+++ views/unaward.php
@@ -5,14 +5,14 @@ echo '<div id="Badges" class="Box Badges">';
 echo '<div class="DismissMessage InfoMessage">'.t('Click on the badge you want to take away from this user.').'</div>';
 echo '<div class="PhotoGrid">';
 echo wrap(sprintf(t('Badges of %s'), userAnchor(Gdn::userModel()->getID($this->data('UserID')))), 'h4');
-foreach($this->data('Badges') as $Badge) {
+foreach($this->data('Badges') as $badge) {
     echo anchor(
         img(
-            $Badge['Photo'],
+            $badge['Photo'],
             array('class' => 'ProfilePhoto')
         ),
-        'yaga/badge/unaward/'.$Badge['BadgeAwardID'],
-        array('title' => $Badge['Name'], 'class' => 'Popup', 'id' => 'BadgeAward-'.$Badge['BadgeAwardID'])
+        'yaga/badge/unaward/'.$badge['BadgeAwardID'],
+        array('title' => $badge['Name'], 'class' => 'Popup', 'id' => 'BadgeAward-'.$badge['BadgeAwardID'])
     );
 }
 echo '</div>';
