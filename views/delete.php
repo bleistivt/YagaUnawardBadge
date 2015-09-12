9d75d9e52fbb999a1e548a91109b596a7df0eb22
bleistivt <bleistivt@users.noreply.github.com>
2015-09-12 14:54:47 +0200

    format

--- views/delete.php
+++ views/delete.php
@@ -1,15 +1,21 @@
 <?php if (!defined('APPLICATION')) exit(); 
 
-echo wrap($this->data('Title'), 'h1');
-echo $this->Form->open();
-echo $this->Form->errors();
-echo '<div class="P">'.sprintf(
-    T('Are you sure you want to take away <em style="font-style:italic;">%s</em> from %s? If the the conditions for receiving this badge are still met, it will be awarded again.'),
-    $this->data('Badgename'),
-    $this->data('Username')
-).'</div>';
-echo '<div class="Buttons Buttons-Confirm">';
-echo $this->Form->button('OK', array('class' => 'Button Primary'));
-echo $this->Form->button('Cancel', array('type' => 'button', 'class' => 'Button Close'));
-echo '</div>';
-echo $this->Form->close();
+echo wrap($this->data('Title'), 'h1'),
+
+    $this->Form->open(),
+    $this->Form->errors(),
+
+    '<div class="P">',
+    sprintf(
+        T('Are you sure you want to take away <em style="font-style:italic;">%s</em> from %s? If the the conditions for receiving this badge are still met, it will be awarded again.'),
+        $this->data('Badgename'),
+        $this->data('Username')
+    ),
+    '</div>',
+
+    '<div class="Buttons Buttons-Confirm">',
+    $this->Form->button('OK', ['class' => 'Button Primary']),
+    $this->Form->button('Cancel', ['type' => 'button', 'class' => 'Button Close']),
+    '</div>',
+
+    $this->Form->close();
