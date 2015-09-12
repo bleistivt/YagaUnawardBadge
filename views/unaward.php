19c0c234ab575cc956f8da1a7051a650473bce95
bleistivt <bleistivt@users.noreply.github.com>
2015-09-12 14:51:02 +0200

    format

--- views/unaward.php
+++ views/unaward.php
@@ -1,19 +1,33 @@
 <?php if (!defined('APPLICATION')) exit(); 
 
 echo wrap($this->data('Title'), 'h1');
-echo '<div id="Badges" class="Box Badges">';
-echo '<div class="DismissMessage InfoMessage">'.t('Click on the badge you want to take away from this user.').'</div>';
-echo '<div class="PhotoGrid">';
-echo wrap(sprintf(t('Badges of %s'), userAnchor(Gdn::userModel()->getID($this->data('UserID')))), 'h4');
+?>
+
+<div id="Badges" class="Box Badges">
+<div class="DismissMessage InfoMessage">
+    <?php echo t('Click on the badge you want to take away from this user.'); ?>
+</div>
+
+<div class="PhotoGrid">
+<?php
+
+echo wrap(sprintf(
+    t('Badges of %s'),
+    userAnchor(Gdn::userModel()->getID($this->data('UserID')))
+), 'h4');
+
 foreach($this->data('Badges') as $badge) {
     echo anchor(
-        img(
-            $badge['Photo'],
-            array('class' => 'ProfilePhoto')
-        ),
+        img($badge['Photo'], ['class' => 'ProfilePhoto']),
         'yaga/badge/unaward/'.$badge['BadgeAwardID'],
-        array('title' => $badge['Name'], 'class' => 'Popup', 'id' => 'BadgeAward-'.$badge['BadgeAwardID'])
+        [
+            'title' => $badge['Name'],
+            'class' => 'Popup',
+            'id' => 'BadgeAward-'.$badge['BadgeAwardID']
+        ]
     );
 }
-echo '</div>';
-echo '</div>';
+?>
+
+</div>
+</div>
